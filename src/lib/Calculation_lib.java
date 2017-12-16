40c702a050a9beb8e96d26051d39134602bbae8c
50616022 <50616022@192.168.0.5>
2017-12-16 11:05:14 +0900

    Ｆｒａｍｅ

---
+++ src/lib/Calculation_lib.java
@@ -0,0 +1,30 @@
+package lib;
+
+public class Calculation_lib {
+private double m,n;
+
+public Calculation_lib(double m, double n) {
+	this.m = m;
+	this.n = n;
+}
+ public double getPlus() {
+	 return this.m + this.n;
+	 
+ }
+ public double getMinus() {
+	 return this.m - this.n;
+	 
+ }
+ public double getTime() {
+	 return this.m * this.n;
+	 
+ }
+ public double getDivide() {
+	 return this.m / this.n;
+	
+ }
+ public double getMod() {
+	 return this.m % this.n;
+	 
+ }
+}
